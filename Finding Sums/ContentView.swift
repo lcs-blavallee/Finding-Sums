10b926ad422c8feaaad099fff05a8536a8cc9460
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 09:58:11 -0500

    Finished addition part

--- Finding Sums/ContentView.swift
+++ Finding Sums/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Stored propterties
+    @State var firstnumber: Int = 1
+    @State var secondnumber: Int = 1
+    
+    //MARK: Computed properties
+    var sum: Int {
+        return firstnumber + secondnumber
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("\(firstnumber)")
+                        .font(.system(size: 44))
+                }
+                Stepper(value: $firstnumber, label:{
+                    Text("select first number")
+                })
+                HStack {
+                    Text("+")
+                        .font(.system(size: 44))
+                    Spacer()
+                    Text("\(secondnumber)")
+                        .font(.system(size: 44))
+                }
+                Stepper(value: $secondnumber, label:{
+                    Text("select second number")
+                })
+                HStack {
+                    Spacer()
+                    Text("\(sum)")
+                        .font(.system(size: 44))
+                }
+            }
+            Spacer()
         }
         .padding()
     }

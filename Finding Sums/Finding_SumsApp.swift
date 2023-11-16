c4884018afdf99eed66e8e16ea97df6c080a51f1
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 10:10:17 -0500

    Added the tab items in the respective specifications and colour scheme

--- Finding Sums/Finding_SumsApp.swift
+++ Finding Sums/Finding_SumsApp.swift
@@ -11,7 +11,40 @@ import SwiftUI
 struct Finding_SumsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView(selection: Binding.constant(3)) {
+                
+                ContentView()
+                    .tabItem {
+                        Image(systemName: "plus")
+                        Text("Plus")
+                    }
+                    .tag(1)
+                
+                        Text("Minus")
+                     .tabItem {
+                        Image(systemName: "minus")
+                        Text("Minus")
+                    }
+                     .tag(2)
+                
+                Text("Multiply")
+                    .tabItem {
+                        Image(systemName: "multiply")
+                        Text("Multiply")
+                    }
+                    .tag(3)
+                
+                Text("Divide")
+                    .tabItem{
+                        Image(systemName: "divide")
+                        Text("Divide")
+                    }
+                    .tag(4)
+            }
+            
+            .accentColor(.purple)
+            .preferredColorScheme(.light)
         }
     }
 }
+

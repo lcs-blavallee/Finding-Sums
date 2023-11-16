844c2f3c04dbf7c48173766772d04ce55d41fb45
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 09:48:28 -0500

    Initial Commit

---
+++ Finding Sums/Finding_SumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finding_SumsApp.swift
+//  Finding Sums
+//
+//  Created by Benjamin Lavallee on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Finding_SumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
